75f88a34fb6edada0d139dbd114da65eb923a335
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-03-29 10:05:55 -0500

    done except for bonus

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # liri-node-app
+LiriBot
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+When Liri searches for spotify songs
+
+When 
+ * Title of the movie.
+  * Year the movie came out.
+  * IMDB Rating of the movie.
+  * Rotten Tomatoes Rating of the movie.
+  * Country where the movie was produced.
+  * Language of the movie.
+  * Plot of the movie.
+  * Actors in the movie.
--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify =  require("node-spotify-api");
 var axios = require("axios");
+var fs = require("fs");
 
 //concert-this
 function concertThis(artistName) {
@@ -12,7 +13,11 @@ function concertThis(artistName) {
         function (response) {
             console.log("Name of Venue: " + response.data[0].venue.name);
             console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country );
-            console.log("Date of the event: " + response.data[0].datatime);
+            console.log("Date of the event: " + response.data[0].datetime);
+            //logs to a file
+         //   fs.appendFile('log.txt', '\n' + response.data[0].venue.name);
+           // fs.appendFile('log.txt', '\n' + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country );
+           // fs.appendFile('log.txt', '\n' +  response.data[0].datatime);
         }
     )
 } 
@@ -32,7 +37,7 @@ function spotifyThisSong(songName) {
         query: songName
     }, function(error, data) {
         if (error) {
-            logIt('Error: ' + error);
+            console.log('Error: ' + error);
             return;
         } 
         else {
@@ -40,6 +45,11 @@ function spotifyThisSong(songName) {
             console.log("Song: " + data.tracks.items[0].name);
             console.log("Preview Link: " + data.tracks.items[3].preview_url);
             console.log("Album: " + data.tracks.items[0].album.name);
+            //save in file
+          //  fs.appendFile('log.txt', '\n' + "Artist: " + data.tracks.items[0].artists[0].name);
+            //fs.appendFile('log.txt', '\n' + "Song: " + data.tracks.items[0].name);
+           // fs.appendFile('log.txt', '\n' + "Preview Link: " + data.tracks.items[3].preview_url);
+           // fs.appendFile('log.txt', '\n' + "Album: " + data.tracks.items[0].album.name);
         }
     });
   };
@@ -64,6 +74,16 @@ function movieThis(movieName) {
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Actors/Actresses: " + response.data.Actors);
+            //logs information in file
+            // fs.appendFile('log.txt', '\n' + "Title: " + response.data.Title);
+            // fs.appendFile('log.txt', '\n' + "Release Year: " + response.data.Year);
+            // fs.appendFile('log.txt', '\n' + "IMDB Rating: " + response.data.imdbRating);
+            // fs.appendFile('log.txt', '\n' + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            // fs.appendFile('log.txt', '\n' + "Country Produced: " + response.data.Country);
+            // fs.appendFile('log.txt', '\n' + "Language: " + response.data.Language);
+            // fs.appendFile('log.txt', '\n' + "Plot: " + response.data.Plot);
+            // fs.appendFile('log.txt', '\n' + "Plot: " + response.data.Plot);
+            // fs.appendFile('log.txt', '\n' + "Actors/Actresses: " + response.data.Actors);
         }
     );
 }
@@ -77,7 +97,7 @@ function doWhatItSays(){
 }
 
 //This function is called to choose which kind of function to choose based on arguments passed to it
-function somthingThis(choose, query) {
+function somethingThis(choose, query) {
     //if statements to choose the funtion
     if (choose === "concert-this"){
         concertThis(query); 
@@ -85,32 +105,20 @@ function somthingThis(choose, query) {
     else if (choose === "spotify-this-song"){
         spotifyThisSong(query);
     }
-    else if (choose === "movies-this"){
+    else if (choose === "movie-this"){
         movieThis(query);
     }
     else if (choose === "do-what-it-says"){
         doWhatItSays(query);
     }
+    else {
+        console.log("Not sure what you are trying to do here, buddy.")
+    }
 }
 
 
-//;pick out the arguments from the command line then choose what function
+//pick out the arguments from the command line then choose what function
 var chooseThis = process.argv[2];
 var queryThis = process.argv[3];
 somethingThis(chooseThis, queryThis);
 
-
-
-
-
-
-
-
-
-
-// BONUS
-// In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
-
-// Make sure you append each command you run to the log.txt file.
-
-// Do not overwrite your file each time you run a command.
